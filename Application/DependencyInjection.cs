46c3fd038a7e088af7dc2ad81b2e0ce1bf9fdce8
mham7 <m.hammadcr7777@gmail.com>
2024-01-24 18:42:33 +0500

    Add project files.

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.Extensions.DependencyInjection;
+namespace Application
+{
+    public static class DependencyInjection
+    {
+
+        //public static IserviceCollection AddApplication()
+        //{
+
+        //}
+    }
+}
