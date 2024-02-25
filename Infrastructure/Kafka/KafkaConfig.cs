f5810707fd5ea544c7b524ff2d26693e2a2e20be
mham7 <m.hammadcr7777@gmail.com>
2024-02-26 00:38:03 +0500

    docker

---
+++ Infrastructure/Kafka/KafkaConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using ServiceStack.Text;
+
+namespace Infrastructure.Kafka
+{      public class KafkaConfig
+        {
+            public required string BootstrapServers { get; set; }
+            public required string ClientId { get; set; }
+            public SecurityProtocol SecurityProtocol { get; set; }
+
+        }
+
+    
+}
---
+++ Infrastructure/Kafka/KafkaConsumerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Kafka
+{
+    public class KafkaConsumerFactory
+    {
+        private readonly KafkaConfig _config;
+
+        public KafkaConsumerFactory(KafkaConfig config)
+        {
+            _config = config;
+        }
+
+        public IConsumer<string, string> CreateConsumer(string topic)
+        {
+            var consumerConfig = new ConsumerConfig
+            {
+                GroupId = "my-consumer-group", 
+                BootstrapServers = _config.BootstrapServers,
+                AutoOffsetReset = AutoOffsetReset.Earliest, 
+                SecurityProtocol = _config.SecurityProtocol
+            };
+
+            return new ConsumerBuilder<string, string>(consumerConfig).Build();
+        }
+    }
+
+}
